a2f953f6f1e53387e61884bb44da0f59c21491a3
frivolta <rivoltafilippo@gmail.com>
2019-09-12 23:25:59 +0200

    :white_check_mark: Adding test of button click and simulate change

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { appointmentTimeOfDay } from '../helpers/appointments.helper';
 
 export const Appointment = ({ customer }) => {
   return (
     <div>
       {customer.firstName}
+    </div>
+  )
+}
+
+export const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0)
+  return (
+    <div id="appointmentsDayView">
+      <ol>
+        {appointments.map((appointment, i) =>
+          <li key={i}>
+            <button type="button" onClick={() => setSelectedAppointment(i)}>
+              {appointmentTimeOfDay(appointment.startsAt)}
+            </button>
+          </li>)}
+      </ol>
+      {appointments.length === 0 ? <p>There are no appointments scheduled for today</p> : <Appointment customer={appointments[selectedAppointment].customer} />}
     </div>
   )
 }
